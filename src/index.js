fe78267daa182d53e0bd9e1ea39ece3587fbdcdd
nogo0d <jmonteiro205@gmail.com>
2017-10-01 15:56:07 +0100

    Trigger aptoide plugin only with aptoide keyword, and try to Fix '@' problem in windows. (Not very confident)

--- src/index.js
+++ src/index.js
@@ -15,31 +15,31 @@ const aptoidePlugin = ({ term, actions, display }) => {
 		actions.hideWindow()
 	};
 
-	let match = term.match(/^aptoide\s+(.{2,})/i);
+	let match = term.match(/^aptoide\s*(.*)/i);
 
 	if (match){
 
-		let term = match[1];
+		let term_query = match[1];
 		let store = null;
 
-		let match_store = term.match(/(.{2,})@(.+)/i);
+		let match_store = term_query.match(/(.{2,})@(.+)/i);
 		if (match_store) {
-			term = match_store[1];
+			term_query = match_store[1];
 			store = match_store[2];
 		} else {
-			let almost_match_store = term.match(/(.{2,})@$/i);
+			let almost_match_store = term_query.match(/(.{2,})@$/i);
 			if (almost_match_store) {
-				term = almost_match_store[1];
+				term_query = almost_match_store[1];
 			}
 		}
 
-		let title = `Search Aptoide for '${term}'`;
+		let title = `Search Aptoide for '${term_query}'`;
 
 		display({
 			icon,
 			title: title,
-			onSelect: () => goSearch(term, store),
-			getPreview: () => <Preview query={term} store={store} actions={actions} />
+			onSelect: () => goSearch(term_query, store),
+			getPreview: () => <Preview query={term_query} store={store} actions={actions} />
 
 		});
 	}
